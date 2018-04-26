f362512c7364c47348ec7ba53703ac19349b1cf7
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-04-26 04:46:01 -0500

    Add generate songs

--- src/models/models.py
+++ src/models/models.py
@@ -3,19 +3,21 @@
 import os
 import numpy as np
 import tensorflow as tf
+from utils.midi_to_matrix import noteStateMatrixToMidi
 
 
 class RNN(object):
     """LSTM model."""
 
-    def __init__(self, model='lstm', ndims=156, num_layers=2, num_units=128, dropout=True, sequence_length=3):
+    def __init__(self, model='lstm', ndims=156, num_layers=2, num_units=128, dropout=True):
         """Initialize model.
 
         Args:
 
         """
-        # Input images
-        self.x_placeholder = tf.placeholder(tf.float32, [None, sequence_length, ndims])
+        # Input and targets
+        self.x_placeholder = tf.placeholder(tf.float32, [None, ndims])
+        self.target_placeholder = tf.placeholder(tf.float32, [None])
 
         # Define RNN hyper-parameters
         self._ndims = ndims
@@ -29,13 +31,13 @@ class RNN(object):
         self.output_keep_prob = tf.placeholder(tf.float32, [])
 
         # Build computational graph
-        logits = self._sequential(self.x_placeholder)
+        logits = self._encoder(self.x_placeholder)
 
         # Compute loss
-        self.cost = self._sequential_loss(logits, y)
+        self.cost = self._encoder_loss(logits, y)
 
         # Add optimizers for appropriate variables
-        self.d_optimizer = tf.train.RMSPropOptimizer(
+        self.optimizer = tf.train.AdamOptimizer(
             learning_rate=self.learning_rate_placeholder).minimize(
             self.cost)
 
@@ -43,18 +45,17 @@ class RNN(object):
         self.session = tf.InteractiveSession()
         self.session.run(tf.global_variables_initializer())
 
-    def _sequential(self, x):
-        """Build sequential model.
+    def _encoder(self, x):
+        """Build sequential encoder model.
 
         Args:
 
         Returns:
 
         """
-        # get lasting time for particular MIDI files.
-        batch_size, self._time, ndims = x.shape
+        batch_size, ndims = x.shape
 
-        # Define helper function to declare layer class
+        # Declare Layer Class
         if self._model == 'rnn':
             rnn_layer = tf.contrib.rnn.BasicRNNCell
         elif self._model == 'gru':
@@ -77,7 +78,7 @@ class RNN(object):
 
         # Simulate the recurrent network over the time
         # outputs is a tensor of shape [batch_size, max_time, cell_state_size]
-        # which is [batch_size, sequence_length, hidden_dim]
+        # => [batch_size, sequence_length, hidden_dim]
         outputs, _ = tf.nn.dynamic_rnn(cell, x, dtype=tf.float32)
 
         # outputs = tf.reshape(outputs, [-1, self._num_units])
@@ -88,28 +89,28 @@ class RNN(object):
         weight, bias = self._weight_and_bias(self._num_units, self._time)
         logits = tf.nn.softmax(tf.matmul(last, weight) + bias)
 
-        # output = tf.reshape(output, [-1, self._num_units])
-        # # output = tf.transpose(output, [1, 0, 2])
-        # last = tf.gather(output, int(output.get_shape()[0]) - 1)
-
-        # # Softmax layer.
-        # weight, bias = self._weight_and_bias(self._num_units, self._time)
-        # prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
-        # return prediction
-
-        # weights = tf.Variable(tf.truncated_normal([self._num_units, vocab_size + 1]))
-        # bias = tf.Variable(tf.zeros(shape=[vocab_size + 1]))
-        # logits = tf.nn.bias_add(tf.matmul(output, weights), bias=bias)
         return logits
 
-    def _sequential_loss(self, logits, gt):
-        # pass
+    def _encoder_loss(self, logits, gt):
+        """Calculate loss of RNN.
 
-        cross_entropy = tf.reduce_sum(gt * tf.log(tf.clip_by_value(logits, 1e-10, 1.0)))
+        Args:
+
+        Returns:
+
+        """
+        cross_entropy = tf.reduce_sum(
+            gt * tf.log(tf.clip_by_value(logits, 1e-10, 1.0)))
         return cross_entropy
 
-    def _generator():
+    def _decoder():
+        """Build sequential decoder model."""
         pass
+        outputs, state = tf.contrib.legacy_seq2seq.rnn_decoder(decoder_inputs,
+                                                               initial_state,
+                                                               cell,
+                                                               loop_function=None,
+                                                               scope=None)
 
     def _load(self, checkpoint_dir):
         """Load model to resume training.
@@ -143,3 +144,15 @@ class RNN(object):
         weight = tf.truncated_normal([in_size, out_size], stddev=0.01)
         bias = tf.constant(0.1, shape=[out_size])
         return tf.Variable(weight), tf.Variable(bias)
+
+def generate_songs(self):
+    """Generate new songs from Gibbs Sampling"""
+    # Now the model is fully trained, so let's make some music!
+    # Run a gibbs chain where the visible nodes are initialized to 0
+    sample = gibbs_sample(1).eval(session=self.session, feed_dict={x: np.zeros((50, n_visible))})
+    for i in range(sample.shape[0]):
+        if not any(sample[i, :]):
+            continue
+        # Here we reshape the vector to be time x notes, and then save the vector as a midi file
+        new_song = np.reshape(sample[i, :], (num_timesteps, 2 * note_range))
+        noteStateMatrixToMidi(new_song, "generated_chord_{}".format(i))
